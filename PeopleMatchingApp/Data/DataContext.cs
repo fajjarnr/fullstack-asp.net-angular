6267db6391c83ed4263e6395c0432030ee3d0cb6
fajjarnr <zfajart@gmail.com>
2022-03-31 16:55:17 +0700

    first commit

---
+++ PeopleMatchingApp/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PeopleMatchingApp.Entities;
+
+namespace PeopleMatchingApp.Data;
+
+public class DataContext : DbContext
+{
+    protected readonly IConfiguration Configuration;
+
+    public DataContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        // connect to mysql with connection string from app settings
+        var connectionString = Configuration.GetConnectionString("WebApiDatabase");
+        options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+    }
+
+    public DbSet<AppUser> Users { get; set; }
+}
