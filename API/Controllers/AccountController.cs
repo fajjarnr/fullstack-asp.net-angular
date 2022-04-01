ffadce1df8c4336cec04e580b3f3267bb4cb1333
fajjarnr <zfajart@gmail.com>
2022-04-02 06:19:46 +0700

    login api

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -11,7 +11,7 @@ namespace API.Controllers;
 public class AccountController : BaseApiController
 {
     private readonly DataContext _context;
-    
+
     public AccountController(DataContext context)
     {
         _context = context;
@@ -33,6 +33,25 @@ public class AccountController : BaseApiController
 
         _context.Users.Add(user);
         await _context.SaveChangesAsync();
+
+        return user;
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+    {
+        var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+
+        if (user == null) return Unauthorized("Invalid username");
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
+        }
 
         return user;
     }
